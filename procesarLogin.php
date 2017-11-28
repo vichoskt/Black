e507bb5de8b50506c008c2ced52a23e8865e38f4
sebaxwtf <giokarem@yahoo.es>
2017-11-28 15:33:12 -0400

    fgg
    tula

--- procesarLogin.php
+++ procesarLogin.php
@@ -26,7 +26,7 @@ if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         
-        echo "<script>alert('vixo qlo. Bienvenido: ".$row["nombre"]."');</script>";
+        echo "<script>alert('Bienvenido: ".$row["nombre"]."');</script>";
     	echo "<script>window.location = 'index2.html';</script>";
     }
 } else {
